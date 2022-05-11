ad793fd0b52f8dfd0542db2e413be3733a0f975c
webbyhead <thompson.andre@gmail.com>
2022-05-11 12:13:17 -0400

    Add project files.

---
+++ AndreFirstMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AndreFirstMVC.Models;
+
+namespace AndreFirstMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            DogViewModel AndreDog = new DogViewModel() { Name = "Bruno", Age = 3 };
+            return View(AndreDog);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public string hello()
+        { return "Hellow this is Andre!!"; }
+    }
+}
---
+++ AndreFirstMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AndreFirstMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
